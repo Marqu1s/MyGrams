0d252ed687fdf76d6880c20056c18ece03e28dba
Brasco <brasco@thebrascode.com>
2016-03-01 20:15:38 -0500

    Brasco's version of alphabetical order

--- AlphabeticOrder.rb
+++ AlphabeticOrder.rb
@@ -1,22 +1,20 @@
 #sort words captured from user in alphabetical order
 
-puts 'Enter a list of words and I will sort them for you in 
-Alphabetical Order'
+puts # skip a line for visual formatting
+puts 'Enter a list of words and I will sort them for you in Alphabetical Order.'
+puts 'Separate each word with a single space:'
 
-words = []
+words = gets.chomp.split
 
-while true
-	word = gets.chomp
-	words.push word
-
-	if word == ''.chomp
-		
-		puts 'Your words in alphabetical order'
-		puts words.sort
-		break
-	end
-
-end
+puts # skip a line for visual formatting
+puts 'Your words in alphabetical order'
+puts # skip a line for visual formatting
+puts words.sort
 
 
-
+# Note: This could have been done in only 4 lines of code - the power of Ruby
+#
+# puts 'Enter a list of words and I will sort them for you in Alphabetical Order. Separate each word with a single space:'
+# words = gets.chomp.split
+# puts 'Your words in alphabetical order'
+# puts words.sort
